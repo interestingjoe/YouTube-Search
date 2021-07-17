c79f0d5fd9a7f59332e1a209d3d0857800c99cd4
Your Name <you@example.com>
2021-07-16 19:31:42 -0700

    Creating most of the logic.

--- dest/js/index.js
+++ dest/js/index.js
@@ -1,10 +1,73 @@
 (() => {
     let main = {
         init: () => {
-            main.hello();
+            main.search();
         },
-        hello: () => {
-            console.log("Test");
+        search: () => {
+            let outputElem = document.getElementsByClassName('output')[0];
+            let api = 'https://content.googleapis.com/youtube/v3/search?part=snippet';
+            let query = '';
+
+            $('#button').off();
+            $('#button').on('click', () => {
+                let input = document.getElementsByClassName('input')[0];
+
+                if (input.value === '') {
+                    return;
+                }
+                outputElem.innerHTML = 'Loading...';
+                query = '&q=' + encodeURIComponent(input.value.trim());
+
+                main.fetchAPI(api + '&key=' + config.apiKey + query)
+                    .then(response => {
+                        outputElem.innerHTML = '';
+                        console.log(response.items);
+                        main.output(response.items);
+                    })
+                    .catch(() => 'Error fetching results');
+            });
+        },
+        fetchAPI: async (api) => {
+            if (api === undefined || api === '') {
+                return;
+            }
+
+            let response = await fetch(api);
+            let json = await response.json();
+            return json;
+        },
+        output: data => {
+            let outputElem = document.getElementsByClassName('output')[0];
+            let renderElem = (li, tag, attr, data) => {
+                let elem = document.createElement(tag);
+                elem.classList.add(attr);
+
+                if (tag === 'img') {
+                    elem.setAttribute('src', data);
+                } else {
+                    elem.innerHTML = data;
+                }
+
+                li.appendChild(elem);
+            }
+
+            if (data === undefined || data === {}) {
+                return;
+            }
+
+            let ul = document.createElement('ul');
+            ul.classList.add('list');
+            outputElem.appendChild(ul);
+
+            for (const key in data) {
+                let li = document.createElement('li');
+
+                renderElem(li, 'img', 'thumbnail', `${data[key]['snippet']['thumbnails']['default']['url']}`);
+                renderElem(li, 'h3', 'title', `${data[key]['snippet']['title']}`);
+                renderElem(li, 'p', 'description', `${data[key]['snippet']['description']}`);
+
+                ul.appendChild(li);
+            }
         }
     };
 
