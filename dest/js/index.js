3bc8ec892a965f58a1564f9bae7d236f78f991dd
Your Name <you@example.com>
2021-07-17 18:49:05 -0700

    Create logic for fetching Tags.

--- dest/js/index.js
+++ dest/js/index.js
@@ -5,8 +5,8 @@
         },
         search: () => {
             let outputElem = document.getElementsByClassName('output')[0];
-            let api = 'https://content.googleapis.com/youtube/v3/search?part=snippet';
-            let query = '';
+            let api = 'https://content.googleapis.com/youtube/v3/';
+            let paramPart = 'part=snippet';
 
             $('#button').off();
             $('#button').on('click', () => {
@@ -16,21 +16,50 @@
                     return;
                 }
                 outputElem.innerHTML = 'Loading...';
-                query = '&q=' + encodeURIComponent(input.value.trim());
+                let url = `${api}search?${paramPart}&key=${config.apiKey}&q=${encodeURIComponent(input.value.trim())}`;
 
-                main.fetchAPI(api + '&key=' + config.apiKey + query)
+                main.fetchAPI(url)
                     .then(response => {
                         if (response.items.length > 0) {
                             outputElem.innerHTML = '';
-                            console.log('1st', response.items);
                             main.output(response.items);
                             return response.items;
                         } else {
                             outputElem.innerHTML = 'No matching videos';
                         }
                     })
-                    .then(res => {
-                        console.log('2nd', res);
+                    .then(async response => {
+                        let arr = [];
+                        for (const key in response) {
+                            if (response[key]['id']['videoId'] === undefined) {
+                                continue;
+                            }
+                            let url = `${api}videos?${paramPart}&key=${config.apiKey}&id=${response[key]['id']['videoId']}`;
+                            let item = await main.fetchAPI(url);
+                            arr.push(item);
+                        }
+                        return await Promise.all(arr);
+                    })
+                    .then(response => {
+                        if (response.length > 0) {
+                            for (const key in response) {
+                                if (
+                                    response[key]['items'][0]['id'] === undefined || 
+                                    (response[key]['items'][0]['snippet']['tags'] === undefined || response[key]['items'][0]['snippet']['tags'] === [])
+                                ) {
+                                    continue;
+                                }
+                                let p = document.createElement('p');
+                                p.classList.add('tags');
+                                let copy = response[key]['items'][0]['snippet']['tags'];
+                                p.innerHTML = '<strong>Video Tags:</strong> ' + copy.toString();
+
+                                let li = document.querySelector(`[data-id='${response[key]['items'][0]['id']}'`);
+                                li.appendChild(p);
+                            }
+                        } else {
+                            console.log('No available tags');
+                        }
                     })
                     .catch(() => {
                         outputElem.innerHTML = 'Error fetching data';
@@ -71,15 +100,16 @@
 
             for (const key in data) {
                 let li = document.createElement('li');
+                li.setAttribute('data-id', `${data[key]['id']['videoId']}`);
 
                 let a = document.createElement('a');
                 a.setAttribute('href', `https://www.youtube.com/watch?v=${data[key]['id']['videoId']}`);
+                li.appendChild(a);
 
                 renderElem(a, 'img', 'thumbnail', `${data[key]['snippet']['thumbnails']['default']['url']}`);
-                renderElem(a, 'h3', 'title', `${data[key]['snippet']['title']}`);
-                renderElem(a, 'h4', 'description', `${data[key]['snippet']['description']}`);
+                renderElem(li, 'h3', 'title', `${data[key]['snippet']['title']}`);
+                renderElem(li, 'h4', 'description', `${data[key]['snippet']['description']}`);
 
-                li.appendChild(a);
                 ul.appendChild(li);
             }
         }
